243352c9b896643b43d42ba34c9d3449f64a1bae
JungInBaek <bji1527@naver.com>
2024-04-10 12:40:06 +0900

    feat: 스택 구현 연습

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,197 +1,68 @@
 ﻿#include <iostream>
 #include <vector>
 #include <list>
+#include <stack>
 using namespace std;
 
 
-template<typename T>
-class Node
+// Stack (LIFO: Last-In-First-Out, 후입선출)
+
+// vector or list로 구현
+template<typename T, typename Container = vector<T>>
+class Stack
 {
 public:
-	Node() : _prev(nullptr), _next(nullptr), _data(T()) {};
-	Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value) {};
-
-	~Node() {};
-
-public:
-	Node* _prev;
-	Node* _next;
-	T _data;
-};
-
-template<typename T>
-class Iterator
-{
-public:
-	Iterator() : _node(nullptr) {};
-	Iterator(Node<T>* ptr) : _node(ptr) {};
-
-	~Iterator() {};
-
-public:
-	T& operator*()
+	void push(const T& value)
 	{
-		return _node->_data;
+		_container.push_back(value);
 	}
 
-	bool operator==(const Iterator& other)
+	void pop()
 	{
-		return _node == other._node;
+		_container.pop_back();
 	}
 
-	bool operator!=(const Iterator& other)
+	T& top()
 	{
-		return _node != other._node;
+		return _container.back();
 	}
 
-	Iterator& operator++()
+	bool empty()
 	{
-		_node = _node->_next;
-		return *this;
-	}
-
-	Iterator operator++(int)
-	{
-		Iterator temp = *this;
-		_node = _node->_next;
-		return temp;
-	}
-
-	Iterator& operator--()
-	{
-		_node = _node->_prev;
-		return *this;
-	}
-
-	Iterator operator--(int)
-	{
-		Iterator temp = *this;
-		_node = _node->_prev;
-		return temp;
-	}
-
-public:
-	Node<T>* _node;
-};
-
-template<typename T>
-class List
-{
-public:
-	List() : _size(0), _head(new Node<T>()), _tail(new Node<T>())
-	{
-		_head->_next = _tail;
-		_tail->_prev = _head;
-	}
-
-	~List()
-	{
-		while (_size > 0)
-		{
-			pop_back();
-		}
-		delete _head;
-		delete _tail;
-	}
-
-	using iterator = Iterator<T>;
-
-public:
-	Node<T>* AddNode(Node<T>* nextNode, const T& value)
-	{
-		Node<T>* prevNode = nextNode->_prev;
-		Node<T>* node = new Node<T>(value);
-
-		node->_prev = prevNode;
-		node->_next = nextNode;
-		prevNode->_next = node;
-		nextNode->_prev = node;
-
-		_size++;
-		return node;
-	}
-
-	Node<T>* RemoveNode(Node<T>* node)
-	{
-		Node<T>* prevNode = node->_prev;
-		Node<T>* nextNode = node->_next;
-
-		prevNode->_next = nextNode;
-		nextNode->_prev = prevNode;
-
-		delete node;
-		_size--;
-		return nextNode;
+		return _container.empty();
 	}
 
 	int size()
 	{
-		return _size;
+		return _container.size();
 	}
 
-public:
-	void push_back(const T& value)
-	{
-		AddNode(_tail, value);
-	}
-
-	void pop_back()
-	{
-		RemoveNode(_tail->_prev);
-	}
-
-public:
-	iterator begin()
-	{
-		return iterator(_head->_next);
-	}
-
-	iterator end()
-	{
-		return iterator(_tail);
-	}
-
-	iterator insert(iterator it, const T& value)
-	{
-
-		Node<T>* node = AddNode(it._node, value);
-		return iterator(node);
-	}
-
-	iterator erase(iterator it)
-	{
-		Node<T>* node = RemoveNode(it._node);
-		return iterator(node);
-	}
-
-public:
-	Node<T>* _head;
-	Node<T>* _tail;
-	int _size;
+private:
+	//vector<T> _container;
+	//list<T> _container;
+	Container _container;
 };
+
 
 int main()
 {
-	List<int> li;
-	
-	List<int>::iterator eraseIt;
-	for (int i = 0; i < 10; i++)
+	stack<int, list<int>> s;
+
+	// 삽입
+	s.push(1);
+	s.push(2);
+	s.push(3);
+
+	while (s.empty() == false)
 	{
-		if (i == 5)
-		{
-			eraseIt = li.insert(li.end(), i);
-		}
-		else
-		{
-			li.push_back(i);
-		}
+		// 최상위 원소
+		int data = s.top();
+
+		// 최상위 원소 삭제
+		s.pop();
+
+		cout << data << endl;
 	}
 
-	li.pop_back();
-	li.erase(eraseIt);
-
-	for (List<int>::iterator it = li.begin(); it != li.end(); ++it)
-	{
-		cout << (*it) << endl;
-	}
+	int size = s.size();
 }
