7aae0d206e66c6879c73257a875284e46cc7e41e
JungInBaek <bji1527@naver.com>
2024-04-12 15:59:51 +0900

    feat: 큐 구현 연습

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -2,14 +2,81 @@
 #include <vector>
 #include <list>
 #include <stack>
+#include <queue>
 using namespace std;
 
 
-// Stack (LIFO: Last-In-First-Out, 후입선출)
+// Queue (FIFO First-In-First-Out 선입선출)
 
-// vector or list로 구현
-template<typename T, typename Container = vector<T>>
-class Stack
+template<typename T>
+class ArrayQueue
+{
+public:
+	ArrayQueue()
+	{
+		_container.resize(100);
+	}
+
+	void push(const T& value)
+	{
+		// TODO: 다 찼는지 체크
+		if (_size == _container.size())
+		{
+			// 증설
+			int newSize = max(1, _size * 2);
+			vector<T> newData;
+			newData.resize(newSize);
+
+			// 데이터 복사
+			for (int i = 0; i < _size; i++)
+			{
+				newData[i] = _container[_front];
+				_front = (_front + 1) % _container.size();
+			}
+			_container.swap(newData);
+			_front = 0;
+			_back = _size;
+		}
+
+		_container[_back] = value;
+		_back = (_back + 1) % _container.size();
+		_size++;
+	}
+
+	void pop()
+	{
+		// TODO: 비었는지 체크
+		if (!empty())
+		{
+			_front = (_front + 1) % _container.size();
+			_size--;
+		}
+	}
+
+	T& front()
+	{
+		return _container[_front];
+	}
+
+	bool empty()
+	{
+		return _size == 0;
+	}
+
+	int size()
+	{
+		return _size;
+	}
+
+private:
+	vector<T> _container;
+	int _front = 0;
+	int _back = 0;
+	int _size = 0;
+};
+
+template<typename T>
+class ListQueue
 {
 public:
 	void push(const T& value)
@@ -19,12 +86,13 @@ public:
 
 	void pop()
 	{
-		_container.pop_back();
+		_container.erase(_container.begin());
 	}
 
-	T& top()
+	T& front()
 	{
-		return _container.back();
+
+		return _container.front();
 	}
 
 	bool empty()
@@ -38,31 +106,24 @@ public:
 	}
 
 private:
-	//vector<T> _container;
-	//list<T> _container;
-	Container _container;
+	list<T> _container;
 };
-
 
 int main()
 {
-	stack<int, list<int>> s;
+	ArrayQueue<int> q;
 
-	// 삽입
-	s.push(1);
-	s.push(2);
-	s.push(3);
-
-	while (s.empty() == false)
+	for (int i = 0; i < 100; i++)
 	{
-		// 최상위 원소
-		int data = s.top();
-
-		// 최상위 원소 삭제
-		s.pop();
-
-		cout << data << endl;
+		q.push(i);
 	}
 
-	int size = s.size();
+	while (q.empty() == false)
+	{
+		int value = q.front();
+		q.pop();
+		cout << value << endl;
+	}
+
+	int size = q.size();
 }
