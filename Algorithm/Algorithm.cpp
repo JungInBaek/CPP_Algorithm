a2c552c785ea9e86612686fb5c9c014691549fcd
unknown <bji0519@gmail.com>
2024-06-11 15:47:21 +0900

    feat: 기본 정렬 구현

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -9,39 +9,68 @@
 using namespace std;
 
 
+void BubbleSort(vector<int>& v)
+{
+	int rear = v.size();
+	while (rear > 0)
+	{
+		for (int i = 0; i < rear - 1; i++)
+		{
+			if (v[i] > v[i + 1])
+			{
+				int temp = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = temp;
+			}
+		}
+		--rear;
+	}
+}
+
+void SelectionSort(vector<int>& v)
+{
+	for (int i = 0; i < v.size() - 1; i++)
+	{
+		int index = i;
+		for (int j = i + 1; j < v.size(); j++)
+		{
+			if (v[j] < v[index])
+			{
+				index = j;
+			}
+		}
+
+		int temp = v[i];
+		v[i] = v[index];
+		v[index] = temp;
+	}
+}
+
+void InsertionSort(vector<int>& v)
+{
+	for (int i = 0; i < v.size() - 1; i++)
+	{
+		int index = i + 1;
+		int temp = v[index];
+		for (int j = i; j >= 0; j--)
+		{
+			if (v[j] < temp)
+			{
+				break;
+			}
+			v[index] = v[j];
+			index = j;
+		}
+		v[index] = temp;
+	}
+}
+
 int main()
 {
-	BinarySearchTree bst;
+	vector<int> v{ 1, 5, 3, 4, 2 };
+	//std::sort(v.begin(), v.end());
 
-	bst.Insert(30);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(25);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(40);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(50);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Delete(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Delete(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	//BubbleSort(v);
+	//SelectionSort(v);
+	InsertionSort(v);
 }
