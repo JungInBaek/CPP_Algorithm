0a76da1cc60a7b8368774f4c8bfa59e9f89209c4
unknown <bji0519@gmail.com>
2024-07-09 17:39:32 +0900

    feat: Triangle path 풀기

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,53 +1,68 @@
 ﻿#include <iostream>
 #include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <thread>
-#include "BinarySearchTree.h"
-#include "Windows.h"
 
 using namespace std;
 
 
-int cache[100];
-vector<int> seq;
+int N;
+vector<int> _path;
+vector<vector<int>> board;
+vector<vector<int>> cache;
 
-int LIS(int pos)
+int path(int y, int x)
 {
-	int& ret = cache[pos];
-	if (ret != -1)
+	if (y >= N)
+	{
+		return 0;
+	}
+
+	int& ret = cache[y][x];
+	if (ret != 0)
 	{
 		return ret;
 	}
 
-	ret = 1;
-
-	for (int next = pos + 1; next < seq.size(); next++)
+	if (y + 1 >= N)
 	{
-		if (seq[pos] < seq[next])
-		{
-			ret = max(ret, LIS(next) + 1);
-		}
+		return ret += board[y][x];
 	}
 
-	return ret;
+	int path1 = path(y + 1, x);
+	_path[y + 1] = x;
+	ret = path1;
+
+	if (x + 1 >= board[y + 1].size())
+	{
+		return ret += board[y][x];
+	}
+
+	int path2 = path(y + 1, x + 1);
+	if (path2 > path1)
+	{
+		_path[y + 1] = x + 1;
+		ret = path2;
+	}
+
+	return ret += board[y][x];
 }
+
 
 int main()
 {
-	::memset(cache, -1, sizeof(cache));
-	seq = vector<int>{ 10, 1, 9, 2, 5, 7 };
-
-	int start = ::GetTickCount64();
-
-	int len = 0;
-	for (int pos = 0; pos < seq.size(); pos++)
+	board = vector<vector<int>>
 	{
-		len = max(len, LIS(pos));
-	}
+		{ 6 },
+		{ 1, 2 },
+		{ 3, 7, 4 },
+		{ 9, 4, 1, 7 },
+		{ 2, 7, 5, 9, 4 },
+	};
 
-	int end = ::GetTickCount64();
+	N = board.size();
+	_path.resize(N);
+	cache.resize(N, vector<int>(N));
 
-	cout << end - start << "ms" << endl;
+	_path[0] = 0;
+	int ret = path(0, 0);
+	cout << ret << endl;
 }
